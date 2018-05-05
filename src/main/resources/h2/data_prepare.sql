024a1ed7ee6d4912aa5b5fb81c52a833dd98cd55
May <may.yjie@gmail.com>
2018-05-05 14:10:19 +0800

    用spring boot搭建的环境

---
+++ src/main/resources/h2/data_prepare.sql
@@ -0,0 +1,6 @@
+
+/*user*/
+INSERT INTO user_info(user_id, user_name, email, password)
+    VALUES (1000, 'May','may.yjie@gmail.com','123456');
+INSERT INTO user_info(user_id, user_name, email, password)
+VALUES (1001, 'qqqq','may.yjie@gmail.com','22222');
